cc853bc71a32e0e1f68845408013b8bff3a76f8d
DotTejada <dotdudeperson@gmail.com>
2025-12-27 16:14:04 -0600

    Now users should make install iv

--- lua/iv.lua
+++ lua/iv.lua
@@ -8,8 +8,7 @@ end
 
 M.open = function()
     local current_file_path = vim.fn.expand('%:p')
-    local viewer = vim.fn.expand("~/programming/iv/iv")
-    vim.system({viewer, current_file_path}, {text = true}, on_exit)
+    vim.system({"iv", current_file_path}, {text = true}, on_exit)
 end
 
 return M
