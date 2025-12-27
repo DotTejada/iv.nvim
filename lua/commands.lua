14cdbfb97c715338710305e3f12fb64521f1899f
DotTejada <dotdudeperson@gmail.com>
2025-12-27 15:30:46 -0600

    Fixed, now just need to make cleaner

--- lua/commands.lua
+++
@@ -1,3 +0,0 @@
-vim.api.nvim_create_user_command("IVOpen", function()
-    require("iv").open()
-end, {})
--- lua/iv.lua
+++ lua/iv.lua
@@ -8,8 +8,8 @@ end
 
 M.open = function()
     local current_file_path = vim.fn.expand('%:p')
-    local exe = vim.fn.expand("~/programming/iv/iv")
-    vim.system({exe, current_file_path}, {text = true}, on_exit)
+    local viewer = vim.fn.expand("~/programming/iv/iv")
+    vim.system({viewer, current_file_path}, {text = true}, on_exit)
 end
 
 return M
